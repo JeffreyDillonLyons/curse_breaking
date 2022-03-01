0409e77b62dd62c7c82980e3cb2b1ece281dd503
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2022-03-01 09:04:30 +0100

    hamarat's energy model added

---
+++ hamarat_model/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,5 @@
+'''
+Created on Mar 24, 2021
+
+@author: jhkwakkel
+'''
---
+++ hamarat_model/.ipynb_checkpoints/energy_model.-checkpoint.py
@@ -0,0 +1,84 @@
+'''
+Created on 27 Jan 2014
+
+@author: jhkwakkel
+'''
+from ema_workbench import (RealParameter, TimeSeriesOutcome, ema_logging,
+                           MultiprocessingEvaluator, ScalarOutcome,
+                           perform_experiments, CategoricalParameter,
+                           save_results, Policy)
+from ema_workbench.connectors.vensim import VensimModel
+from ema_workbench.em_framework.evaluators import SequentialEvaluator
+
+
+def get_energy_model():
+    model = VensimModel('energyTransition', wd='./models', model_file='RB_V25_ets_1_policy_modified_adaptive_extended_outcomes.vpm')
+    
+    model.outcomes = [TimeSeriesOutcome('cumulative carbon emissions'),
+                      TimeSeriesOutcome('carbon emissions reduction fraction'),
+                      TimeSeriesOutcome('fraction renewables'),
+                      TimeSeriesOutcome('average total costs'),
+                      TimeSeriesOutcome('total costs of electricity'),]
+        
+    model.uncertainties = [RealParameter("year", 0.9, 1.1),                    
+                         RealParameter("demand fuel price elasticity factor", 0, 0.5),                               
+                         RealParameter("economic lifetime biomass", 30, 50),
+                         RealParameter("economic lifetime coal", 30, 50),
+                         RealParameter("economic lifetime gas", 25, 40),
+                         RealParameter("economic lifetime igcc", 30, 50),
+                         RealParameter("economic lifetime ngcc", 25, 40),
+                         RealParameter("economic lifetime nuclear", 50, 70),
+                         RealParameter("economic lifetime pv", 20, 30),
+                         RealParameter("economic lifetime wind", 20, 30),
+                         RealParameter("economic lifetime hydro", 50, 70,),               
+                         RealParameter("uncertainty initial gross fuel costs", 0.5, 1.5),            
+                         RealParameter("investment proportionality constant", 0.5, 4),
+                         RealParameter("investors desired excess capacity investment", 0.2,2),                  
+                         RealParameter("price demand elasticity factor", -0.07, -0.001),               
+                         RealParameter("price volatility global resource markets", 0.1, 0.2),                      
+                         RealParameter("progress ratio biomass", 0.85,1),
+                         RealParameter("progress ratio coal", 0.9,1.05),
+                         RealParameter("progress ratio gas", 0.85,1),
+                         RealParameter("progress ratio igcc", 0.9,1.05),
+                         RealParameter("progress ratio ngcc", 0.85,1),
+                         RealParameter("progress ratio nuclear", 0.9,1.05),
+                         RealParameter("progress ratio pv", 0.75,0.9),
+                         RealParameter("progress ratio wind", 0.85,1),
+                         RealParameter("progress ratio hydro", 0.9,1.05),                                                    
+                         RealParameter("starting construction time", 0.1,3),                    
+                         RealParameter("time of nuclear power plant ban", 2013,2100),
+                         RealParameter("weight factor carbon abatement", 1,10),
+                         RealParameter("weight factor marginal investment costs", 1,10),    
+                         RealParameter("weight factor technological familiarity", 1,10),    
+                         RealParameter("weight factor technological growth potential", 1,10),                  
+                         RealParameter("maximum battery storage uncertainty constant", 0.2,3),
+                         RealParameter("maximum no storage penetration rate wind", 0.2,0.6),
+                         RealParameter("maximum no storage penetration rate pv", 0.1,0.4),
+                         CategoricalParameter("SWITCH lookup curve TGC", (1,2,3,4)),
+                         CategoricalParameter("SWTICH preference carbon curve", (1,2)),
+                         CategoricalParameter("SWITCH economic growth", (1,2,3,4,5,6)), 
+                         CategoricalParameter("SWITCH electrification rate", (1,2,3,4,5,6)),               
+                         CategoricalParameter("SWITCH Market price determination", (1,2)),
+                         CategoricalParameter("SWITCH physical limits", (1,2)), 
+                         CategoricalParameter("SWITCH low reserve margin price markup", (1,2,3,4)), 
+                         CategoricalParameter("SWITCH interconnection capacity expansion", (1,2,3,4)), 
+                         CategoricalParameter("SWITCH storage for intermittent supply", (1,2,3,4,5,6,7)), 
+                         CategoricalParameter("SWITCH carbon cap", (1,2,3), ),  
+                         CategoricalParameter("SWITCH TGC obligation curve", (1,2,3)), 
+                         CategoricalParameter("SWITCH carbon price determination", (1,2,3)),                      
+                          ]   
+    return model    
+
+
+if __name__ == '__main__':
+    ema_logging.log_to_stderr(ema_logging.INFO)    
+    model = get_energy_model()
+    
+    
+    policies = [Policy('adaptive policy', model_file='RB_V25_ets_1_policy_modified_adaptive_extended_outcomes.vpm'),
+                Policy('no policy', model_file='RB_V25_ets_1_extended_outcomes.vpm')]
+    
+    with MultiprocessingEvaluator(model) as evaluator:
+        results = evaluator.perform_experiments(6000, policies=policies)
+
+    save_results(results, './data/6000_both_policies.tar.gz')
